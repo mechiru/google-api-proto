682a13409a95d2db1b9e5e1649047a177494cf9f
mechiru <u9053u6d41@gmail.com>
2021-12-25 02:51:30 +0900

    examples: add pubsub example

---
+++ examples/src/tonic.rs
@@ -0,0 +1,26 @@
+use std::env;
+
+use google_api_proto::google::pubsub::v1::{publisher_client::PublisherClient, ListTopicsRequest};
+use google_authz::GoogleAuthz;
+use tonic::{transport::Channel, Request};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt::init();
+
+    let project = env::args().nth(1).expect("cargo run --bin tonic -- <GCP_PROJECT_ID>");
+    let channel = Channel::from_static("https://pubsub.googleapis.com").connect().await?;
+    let channel = GoogleAuthz::new(channel).await;
+
+    let mut client = PublisherClient::new(channel);
+    let response = client
+        .list_topics(Request::new(ListTopicsRequest {
+            project: format!("projects/{}", project),
+            page_size: 10,
+            ..Default::default()
+        }))
+        .await?;
+    println!("response = {:#?}", response);
+
+    Ok(())
+}
