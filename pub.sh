ac7f5bc734a80dbeb2c117bbe9e27c52dfc06d18
mechiru <mechiru@users.noreply.github.com>
2021-12-27 21:52:44 +0900

    cd: check for changes before code generation

--- pub.sh
+++ pub.sh
@@ -7,35 +7,36 @@ cd $(dirname ${BASH_SOURCE:-$0})
 echo "Update git submodule"
 git submodule update --remote --rebase xtask/proto
 
+if git diff --exit-code --quiet; then
+  echo "No changes!"
+  exit 0
+fi
+
 echo "Run xtask all"
 if ! cargo xtask all; then
   exit 1
 fi
 
-if git diff --exit-code --quiet; then
-  echo "No changes!"
-else
-  if [ ${CI:-false} ]; then
-    git config --local user.name "mechiru"
-    git config --local user.email "mechiru@users.noreply.github.com"
-    git remote remove origin
-    git remote add origin "https://$(git config user.name):${MY_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-  fi
-
-  git add xtask/proto \
-    && git commit -m "xtask: update submodule googleapis/googleapis"
-  git add google-api-proto \
-    && git commit -m "google-api-proto: regenerate code"
-
-  echo "Sync with the origin repository"
-  git push -u origin master
-
-  echo "Publish to crates.io"
-  cargo release \
-        --execute \
-        --no-confirm \
-        --package google-api-proto \
-        --token "$CARGO_REGISTRY_TOKEN" \
-        --verbose \
-        alpha
+if [ ${CI:-false} ]; then
+  git config --local user.name "mechiru"
+  git config --local user.email "mechiru@users.noreply.github.com"
+  git remote remove origin
+  git remote add origin "https://$(git config user.name):${MY_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 fi
+
+git add xtask/proto \
+  && git commit -m "xtask: update submodule googleapis/googleapis"
+git add google-api-proto \
+  && git commit -m "google-api-proto: regenerate code"
+
+echo "Sync with the origin repository"
+git push -u origin master
+
+echo "Publish to crates.io"
+cargo release \
+      --execute \
+      --no-confirm \
+      --package google-api-proto \
+      --token "$CARGO_REGISTRY_TOKEN" \
+      --verbose \
+      alpha
