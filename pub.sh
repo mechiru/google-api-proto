17b943100e26744dbbf1aceae030d981cf0afe0f
mechiru <mechiru@users.noreply.github.com>
2021-12-30 01:14:38 +0900

    cd: check the diffs in the generated code

--- pub.sh
+++ pub.sh
@@ -7,13 +7,18 @@ cd $(dirname ${BASH_SOURCE:-$0})
 echo "Update git submodule"
 git submodule update --remote --rebase xtask/proto
 
+echo "Run xtask clean generate"
+if ! cargo xtask clean generate; then
+  exit 1
+fi
+
 if git diff --exit-code --quiet; then
   echo "No changes!"
   exit 0
 fi
 
-echo "Run xtask all"
-if ! cargo xtask all; then
+echo "Run xtask test"
+if ! cargo xtask test; then
   exit 1
 fi
 
