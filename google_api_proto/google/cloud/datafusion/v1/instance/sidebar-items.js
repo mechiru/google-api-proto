8faa18c81b4169081f30adb192a8ee4dfca26986
mechiru <mechiru@users.noreply.github.com>
2022-01-13 01:40:01 +0000

    deploy: a6155b6c771ac7f225fb6b9f909b471c6bba2bb6

--- google_api_proto/google/cloud/datafusion/v1/instance/sidebar-items.js
+++ google_api_proto/google/cloud/datafusion/v1/instance/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["State","Represents the state of a Data Fusion instance"],["Type","Represents the type of Data Fusion instance. Each type is configured with the default settings for processing and memory."]]});
+initSidebarItems({"enum":[["DisabledReason","The reason for disabling the instance if the state is DISABLED."],["State","Represents the state of a Data Fusion instance"],["Type","Represents the type of Data Fusion instance. Each type is configured with the default settings for processing and memory."]]});
---
+++ google_api_proto/google/cloud/datafusion/v1/version/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["Type","Each type represents the release availability of a CDF version"]]});
