e5dc0db1afba1de15cc8f8fbd45ea2a2adf819c9
mechiru <mechiru@users.noreply.github.com>
2022-01-25 01:39:07 +0000

    deploy: 6f26feb54e5248b83de3379e96b1b655de66c47b

---
+++ google_api_proto/google/container/v1beta1/node_pool/placement_policy/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["Type","Type defines the type of placement policy."]]});
--- google_api_proto/google/container/v1beta1/node_pool/sidebar-items.js
+++ google_api_proto/google/container/v1beta1/node_pool/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Status","The current status of the node pool instance."]],"struct":[["UpgradeSettings",""]]});
+initSidebarItems({"enum":[["Status","The current status of the node pool instance."]],"mod":[["placement_policy","Nested message and enum types in `PlacementPolicy`."]],"struct":[["PlacementPolicy","PlacementPolicy defines the placement policy used by the node pool."],["UpgradeSettings",""]]});
