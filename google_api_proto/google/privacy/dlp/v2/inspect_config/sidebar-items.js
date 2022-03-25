58b1e46a15a0dec5a8456726aecf82f0ec67faa8
mechiru <mechiru@users.noreply.github.com>
2022-03-25 18:45:01 +0000

    deploy: ebecdb1085324fd018b1cb70ecf3f39a5dba2f6d

--- google_api_proto/google/privacy/dlp/v2/inspect_config/sidebar-items.js
+++ google_api_proto/google/privacy/dlp/v2/inspect_config/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["finding_limits","Nested message and enum types in `FindingLimits`."]],"struct":[["FindingLimits","Configuration to control the number of findings returned. Cannot be set if de-identification is requested."]]});
+initSidebarItems({"mod":[["finding_limits","Nested message and enum types in `FindingLimits`."]],"struct":[["FindingLimits","Configuration to control the number of findings returned for inspection. This is not used for de-identification or data profiling."]]});
