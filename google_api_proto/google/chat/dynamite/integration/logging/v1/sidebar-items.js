2d711ce5507fc6f370848c66bcac5f290aed47ba
mechiru <mechiru@users.noreply.github.com>
2022-04-14 18:57:44 +0000

    deploy: a708b8f3d5d75b81de33a371a07d5534c5116a25

--- google_api_proto/google/chat/dynamite/integration/logging/v1/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"struct":[["DynamiteIntegrationLogEntry","JSON payload of error messages. If the Cloud Logging API is enabled, these error messages are logged to Google Cloud Logging."]]});
