f52ee4c00c686b3f36eab96645f3c4870880f8b7
mechiru <mechiru@users.noreply.github.com>
2022-07-20 12:41:45 +0000

    google-api-proto: regenerate code

--- google-api-proto/src/google/ads/googleads/mod.rs
+++ google-api-proto/src/google/ads/googleads/mod.rs
@@ -20,16 +20,6 @@ pub mod v10;
 pub mod v11;
 #[cfg(
     any(
-        feature = "google-ads-googleads-v8-common",
-        feature = "google-ads-googleads-v8-enums",
-        feature = "google-ads-googleads-v8-errors",
-        feature = "google-ads-googleads-v8-resources",
-        feature = "google-ads-googleads-v8-services",
-    )
-)]
-pub mod v8;
-#[cfg(
-    any(
         feature = "google-ads-googleads-v9-common",
         feature = "google-ads-googleads-v9-enums",
         feature = "google-ads-googleads-v9-errors",
