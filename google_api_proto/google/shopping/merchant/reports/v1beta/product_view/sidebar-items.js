2122c6d58446be8f5f9e538113ae5796d39f671e
mechiru <mechiru@users.noreply.github.com>
2024-04-09 18:34:57 +0000

    deploy: 6c669d1b5c1bdd1ef566615e4a9d27c6d4cfb6cb

--- google_api_proto/google/shopping/merchant/reports/v1beta/product_view/sidebar-items.js
+++ google_api_proto/google/shopping/merchant/reports/v1beta/product_view/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["AggregatedReportingContextStatus"],"mod":["item_issue"],"struct":["ItemIssue"]};
+window.SIDEBAR_ITEMS = {"enum":["AggregatedReportingContextStatus","ClickPotential"],"mod":["item_issue"],"struct":["ItemIssue"]};
